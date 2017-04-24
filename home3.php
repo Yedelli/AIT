5bff9e737feb551165798b5e946ccf21c9478adf
Yedelli <rohini.yedelli@ves.ac.in>
2017-04-24 21:32:43 +0530

    added login modal

--- home3.php
+++ home3.php
@@ -8,12 +8,7 @@
     
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
-
 	
-	
-
-	
-
 	<!-- home -->
 	<link rel="stylesheet" href="css/home.css">
 	
@@ -37,7 +32,7 @@
 					<a href="#home">Home</a>					
 					<a href="#news">About</a>
 					<a href="#contact">Contact</a>
-					<a  id="Login">Login</a>
+					<a  id="login">Login</a>
 					<a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>			
 				</div>
 				<div class="w3-col s1 l4">
@@ -100,7 +95,7 @@
 			</div>
 		</div>
 		<div class="w3-row-padding">	
-		<div class="w3-container">
+		
 			<div class="w3-col l3">
 				<div class="polaroid">
 				<img src="Home page pics/mumbai_img.png" style="width: 100%;" >	
@@ -133,7 +128,7 @@
 					</div>
 				</div>
 			</div>
-		</div>
+		
 		</div>
 		<hr>
 		<div class="w3-row-padding">
@@ -141,6 +136,36 @@
 			<div class="w3-col s12 l4">
 				<h2>FAMOUS PHOTOGRAPHERS</h2>			
 			</div>
+			
+			<div class="w3-row-padding">	
+		
+			<div class="w3-col l4">
+				<div class="polaroid">
+				<img src="images/3.gif" style="width: 100%;" >	
+					<div class="container">
+					<p>RR PHOTOGRAPHERS</p>
+					</div>
+				</div>
+			</div>		
+			<div class="w3-col l4">
+				<div class="polaroid">
+				<img src="images/10.gif" style="width: 100%;">	
+					<div class="container">
+					<p>ASHISH STUDIOS</p>
+					</div>
+				</div>
+			</div>
+			<div class="w3-col l4">
+				<div class="polaroid">
+				<img src="images/11.gif" style="width: 100%;">	
+					<div class="container">
+					<p>AWESOME</p>
+					</div>
+				</div>
+			</div>			
+		
+		</div>
+			
 		</div>
 		
 		<hr>
@@ -174,13 +199,48 @@
     </div>
     <div class="modal-body">
      <form class="form" action="" method="post" >
-    <input type="button" class="needPhoto"  href="#login" data-toggle="modal" name="needPhoto" value="I need a Photographer">
+    <input type="button" class="needPhoto"  id="login1" name="needPhoto" value="I need a Photographer">
 	<pre><p align="centre" style="font-size:20px ">         OR</p></pre>
 	<input type="button" id="photographer" name="photographer" value="I am a Photographer   ">
     </form>
 	<br>
     </div>
     
+  </div>
+
+</div>
+
+<!-- The Modal -->
+<div id="myModal1" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <div class="modal-header">
+      <span class="close1">&times;</span>
+      <h2>CLICK MY PIC</h2>
+    </div>
+    <div class="modal-body">
+	
+	<form class="form" action="/action_page.php">
+
+        <div class="w3-section">
+		  
+		  <input class="w3-input w3-border w3-margin-bottom" type="email" placeholder="Enter Email Id" name="email" required style="width:100%"><br><br>
+		  		  
+          <input class="w3-input w3-border" type="password" placeholder="Enter Password" name="password" required style="width:100%"><br><br>
+		  
+          <input class="w3-check w3-margin-top" type="checkbox" checked="checked"> Remember Me <br><br>
+		  
+		  <button class="w3-button w3-block w3-green w3-section w3-padding" id="loginbtn" type="submit">Login</button>
+		  
+		  <p> New to Click My Pic? <a href="signup.html"> Sign Up</a></p>
+
+        </div>
+
+      </form>
+
+      </div>
+   
   </div>
 
 </div>
@@ -202,7 +262,7 @@ function myFunction() {
 var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-var btn = document.getElementById("Login");
+var btn = document.getElementById("login");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -221,6 +281,37 @@ span.onclick = function() {
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+    }
+}
+
+
+
+// Get the modal_login
+var modal1 = document.getElementById('myModal1');
+
+// Get the button that opens the modal
+var btn1 = document.getElementById("login1");
+
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close1")[0];
+
+
+// When the user clicks the button, open the modal 
+btn1.onclick = function() {
+    modal1.style.display = "block";
+}
+
+
+
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+    modal1.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal1) {
+        modal1.style.display = "none";
     }
 }
 
